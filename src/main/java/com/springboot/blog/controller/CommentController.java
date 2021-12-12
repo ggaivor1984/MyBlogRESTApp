fc9d804e6891ae79cce745d3d8c975ee89c6e463
Grigorii Gaivoronskii <ggaivor@gmail.com>
2021-12-12 14:25:11 +0300

    add versioning

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/v1")
 public class CommentController {
     CommentService commentService;
 
@@ -18,25 +18,25 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    @PostMapping("posts/{postId}/comments")
+    @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
                                                     @Valid @RequestBody CommentDto commentDto) {
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
-    @GetMapping("posts/{postId}/comments")
+    @GetMapping("/posts/{postId}/comments")
     public List<CommentDto> getCommentsByPostId(@PathVariable(value = "postId") long postId) {
         return commentService.getCommentsByPostId(postId);
     }
 
-    @GetMapping("posts/{postId}/comments/{commentId}")
+    @GetMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") long postId,
                                                      @PathVariable(value = "commentId") long commentId) {
         CommentDto commentDto = commentService.getCommentById(postId, commentId);
         return new ResponseEntity<>(commentDto, HttpStatus.OK);
     }
 
-    @PutMapping("posts/{postId}/comments/{commentId}")
+    @PutMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") long postId,
                                                     @PathVariable(value = "commentId") long commentId,
                                                     @Valid @RequestBody CommentDto requestComment) {
@@ -44,7 +44,7 @@ public class CommentController {
         return new ResponseEntity<>(commentDto, HttpStatus.OK);
     }
 
-    @DeleteMapping("posts/{postId}/comments/{commentId}")
+    @DeleteMapping("/posts/{postId}/comments/{commentId}")
     public ResponseEntity<String> deleteComment(@PathVariable(value = "postId") long postId,
                                                 @PathVariable(value = "commentId") long commentId) {
         commentService.deleteComment(postId, commentId);
--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostDtoV2;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 
@@ -12,10 +13,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping
 public class PostController {
     PostService postService;
 
@@ -24,12 +26,12 @@ public class PostController {
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -39,20 +41,107 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+//    @GetMapping("/api/v2/posts/{id}")
+//    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id) {
+//        PostDto postDto = postService.getPostById(id);
+//        PostDtoV2 postDtoV2 = new PostDtoV2();
+//        postDtoV2.setId(postDto.getId());
+//        postDtoV2.setTitle(postDto.getTitle());
+//        postDtoV2.setDescription(postDto.getDescription());
+//        postDtoV2.setContent(postDto.getContent());
+//        postDtoV2.setComments(postDto.getComments());
+//        List<String> tags = new ArrayList<>();
+//        tags.add("Java");
+//        tags.add("JWT");
+//        tags.add("MyBlog");
+//        postDtoV2.setTags(tags);
+//
+//        return ResponseEntity.ok(postDtoV2);
+//    }
+
+//    @GetMapping(value = "/api/posts/{id}", params = "version=1")
+//    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id) {
+//        return ResponseEntity.ok(postService.getPostById(id));
+//    }
+//
+//    @GetMapping(value = "/api/posts/{id}", params = "version=2")
+//    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id) {
+//        PostDto postDto = postService.getPostById(id);
+//        PostDtoV2 postDtoV2 = new PostDtoV2();
+//        postDtoV2.setId(postDto.getId());
+//        postDtoV2.setTitle(postDto.getTitle());
+//        postDtoV2.setDescription(postDto.getDescription());
+//        postDtoV2.setContent(postDto.getContent());
+//        postDtoV2.setComments(postDto.getComments());
+//        List<String> tags = new ArrayList<>();
+//        tags.add("Java");
+//        tags.add("JWT");
+//        tags.add("MyBlog");
+//        postDtoV2.setTags(tags);
+//
+//        return ResponseEntity.ok(postDtoV2);
+//    }
+
+//    @GetMapping(value = "/api/posts/{id}", headers = "X-API-VERSION=1")
+//    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id) {
+//        return ResponseEntity.ok(postService.getPostById(id));
+//    }
+//
+//    @GetMapping(value = "/api/posts/{id}", headers = "X-API-VERSION=2")
+//    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id) {
+//        PostDto postDto = postService.getPostById(id);
+//        PostDtoV2 postDtoV2 = new PostDtoV2();
+//        postDtoV2.setId(postDto.getId());
+//        postDtoV2.setTitle(postDto.getTitle());
+//        postDtoV2.setDescription(postDto.getDescription());
+//        postDtoV2.setContent(postDto.getContent());
+//        postDtoV2.setComments(postDto.getComments());
+//        List<String> tags = new ArrayList<>();
+//        tags.add("Java");
+//        tags.add("JWT");
+//        tags.add("MyBlog");
+//        postDtoV2.setTags(tags);
+//
+//        return ResponseEntity.ok(postDtoV2);
+//    }
+
+//    @GetMapping(value = "/api/posts/{id}", produces = "application/vnd.gaivor.v1+json")
+//    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id) {
+//        return ResponseEntity.ok(postService.getPostById(id));
+//    }
+//
+//    @GetMapping(value = "/api/posts/{id}", produces = "application/vnd.gaivor.v2+json")
+//    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id) {
+//        PostDto postDto = postService.getPostById(id);
+//        PostDtoV2 postDtoV2 = new PostDtoV2();
+//        postDtoV2.setId(postDto.getId());
+//        postDtoV2.setTitle(postDto.getTitle());
+//        postDtoV2.setDescription(postDto.getDescription());
+//        postDtoV2.setContent(postDto.getContent());
+//        postDtoV2.setComments(postDto.getComments());
+//        List<String> tags = new ArrayList<>();
+//        tags.add("Java");
+//        tags.add("JWT");
+//        tags.add("MyBlog");
+//        postDtoV2.setTags(tags);
+//
+//        return ResponseEntity.ok(postDtoV2);
+//    }
+
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
         PostDto postResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
         postService.deletePostById(id);
         return new ResponseEntity<>("Post with id " + id + " was successfully deleted from DB", HttpStatus.OK);
