41c64504f203554c4ea2bd0a238bfd04e4926ff5
Grigorii Gaivoronskii <ggaivor@gmail.com>
2021-12-15 18:31:08 +0300

    add options do deploy on heroku

--- src/main/java/com/springboot/blog/SpringbootRestApiBlogApplication.java
+++ src/main/java/com/springboot/blog/SpringbootRestApiBlogApplication.java
@@ -1,12 +1,16 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootRestApiBlogApplication {
+public class SpringbootRestApiBlogApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -18,4 +22,17 @@ public class SpringbootRestApiBlogApplication {
 		SpringApplication.run(SpringbootRestApiBlogApplication.class, args);
 	}
 
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
